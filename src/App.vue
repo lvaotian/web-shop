e016323785b094ffb8116fcf62df2863778948b1
lvaotian <2385933361@qq.com>
2023-08-29 21:12:34 +0800

    ElementPlus配置自动按需导入

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <el-button type="success">Success</el-button>
       </nav>
     </div>
   </header>
