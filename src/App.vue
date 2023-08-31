726f83f8913ffa15e42bc3a0b89f40eedb25ac77
lvaotian <2385933361@qq.com>
2023-08-31 22:03:25 +0800

    scss文件自动导入

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
 
 <template>
   <el-button type="primary">i am button</el-button>
+  <div class="test">test</div>
   <router-view></router-view>
 </template>
 
+<style scoped lang="scss">
+.test{
+  color: $priceColor;
+}
+</style>
