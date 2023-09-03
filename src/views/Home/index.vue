0e3f5408784243824e1182b7a3c545f47b54ebb2
lvaotian <2385933361@qq.com>
2023-09-03 21:46:39 +0800

    banner轮播图

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,12 +1,20 @@
-<template>
-  <div style="height: 1000px;">我是home</div>
-</template>
-
-<script>
-export default {
-
-}
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
 </script>
+
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
+</template>
 
 <style>
 
