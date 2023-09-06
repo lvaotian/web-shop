7be91ac00e6b459abd09eb2d87c610562c471665
lvaotian <2385933361@qq.com>
2023-09-06 15:30:16 +0800

    二级分类-面包屑导航实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,5 +1,15 @@
 <script setup>
-
+import {ref} from 'vue'
+import { getCategoryFilterAPI } from '@/apis/category'
+import { useRoute } from 'vue-router'
+// 获取面包屑导航数据
+const filterData = ref({})
+const route = useRoute()
+const getFilterData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id)
+  filterData.value = res.result
+}
+getFilterData()
 
 </script>
 
@@ -9,9 +19,9 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家
+        <el-breadcrumb-item :to="{ path: `/category/${filterData.parentId}` }">{{ filterData.parentName }}
         </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ filterData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
