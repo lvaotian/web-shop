7a9bac7e37bc0d8ed6c900be1fcf9af96307de71
lvaotian <2385933361@qq.com>
2023-09-07 22:23:24 +0800

    登录-表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 import { ref } from 'vue'
 // 表单数据对象
 const form = ref({
@@ -24,6 +23,17 @@ const rules = {
       }
     }
   ]
+}
+const formRef = ref(null)
+const doLogin = () => {
+  // 调用实例方法
+  formRef.value.validate((valid) => {
+    // valid: 所有表单都通过校验  才为true
+    // 以valid做为判断条件 如果通过校验才执行登录逻辑
+    if (valid) {
+      console.log(valid)
+    }
+  })
 }
 </script>
 
@@ -61,7 +71,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
