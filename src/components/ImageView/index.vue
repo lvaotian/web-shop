3b9175fa75a5b74ba78c0e322098a601266707e6
lvaotian <2385933361@qq.com>
2023-09-07 16:41:39 +0800

    详情页-图片预览组件-props适配和整体总结

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -2,13 +2,12 @@
 import { ref, watch } from 'vue'
 import { useMouseInElement } from '@vueuse/core'
 // 图片列表
-const imageList = [
-  "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-  "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-  "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-  "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-  "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+defineProps({
+  imageList:{
+    type:Array,
+    default:()=>[]
+  }
+})
 // 实现鼠标移入交互
 const curIndex = ref(0)
 const mouseEnterFn = (i) => curIndex.value = i
@@ -70,7 +69,7 @@ watch([elementX, elementY, isOutside], () => {
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
+        backgroundImage: `url(${imageList[curIndex]})`,
         backgroundPositionX: `${positionX}px`,
         backgroundPositionY: `${positionY}px`,
       },
