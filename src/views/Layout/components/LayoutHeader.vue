a65bfb74f6391dcd33c1bd8c0be70190052a6b4e
lvaotian <2385933361@qq.com>
2023-09-08 21:49:35 +0800

    购物车-本地-头部购物车列表渲染

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useCategoryStore } from '@/stores/categoryStore'
 const categoryStore = useCategoryStore()
-
+import HeaderCart from './HeaderCart.vue'
 </script>
 
 <template>
@@ -20,6 +20,7 @@ const categoryStore = useCategoryStore()
         <input type="text" placeholder="搜一搜">
         </div>
       <!-- 头部购物车 -->
+      <HeaderCart />
     </div>
   </header>
 </template>
