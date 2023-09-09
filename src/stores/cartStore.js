8d862f32527eaa02a9949f19a27e3e226f77bc57
lvaotian <2385933361@qq.com>
2023-09-09 20:52:20 +0800

    购物车-接口-加入购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -2,25 +2,31 @@
 
 import { defineStore } from 'pinia'
 import { ref,computed } from 'vue'
-
+import { insertCartAPI } from '@/apis/cart'
+import { useUserStore } from './userStore'
 
 export const useCartStore = defineStore('cart', () => {
+  const userStore = useUserStore()
+  const isLogin = computed(() => userStore.userInfo.token)
   // 1. 定义state - cartList
   const cartList = ref([])
   // 2. 定义action - addCart
-  const addCart = (goods) => {
-    console.log('添加', goods)
-    // 添加购物车操作
-    // 已添加过 - count + 1
-    // 没有添加过 - 直接push
-    // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
-    const item = cartList.value.find((item) => goods.skuId === item.skuId)
-    if (item) {
-      // 找到了
-      item.count++
+  const addCart = async (goods) => {
+    const { skuId, count } = goods
+    // 登录
+    if (isLogin.value) {
+      // 登录之后的加入购车逻辑
+      await insertCartAPI({ skuId, count })
     } else {
-      // 没找到
-      cartList.value.push(goods)
+      // 未登录
+      const item = cartList.value.find((item) => goods.skuId === item.skuId)
+      if (item) {
+        // 找到了
+        item.count++
+      } else {
+        // 没找到
+        cartList.value.push(goods)
+      }
     }
   }
     // 删除购物车
