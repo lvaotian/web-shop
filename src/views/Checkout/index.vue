8562ca6659e11dddeb97051d39836097b2e1eba6
lvaotian <2385933361@qq.com>
2023-09-10 15:57:49 +0800

    结算-生成订单功能实现

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -33,6 +33,33 @@ const confirm = () => {
   activeAddress.value = {}
 }
 
+
+// 创建订单
+const createOrder = async () => {
+  const res = await createOrderAPI({
+    deliveryTimeType: 1,
+    payType: 1,
+    payChannel: 1,
+    buyerMessage: '',
+    goods: checkInfo.value.goods.map(item => {
+      return {
+        skuId: item.skuId,
+        count: item.count
+      }
+    }),
+    addressId: curAddress.value.id
+  })
+  const orderId = res.result.id
+  router.push({
+    path: '/pay',
+    query: {
+      id: orderId
+    }
+  })
+  // 更新购物车
+  cartStore.updateNewList()
+}
+
 </script>
 
 <template>
@@ -127,7 +154,7 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large" >提交订单</el-button>
+          <el-button @click="createOrder" type="primary" size="large">提交订单</el-button>
         </div>
       </div>
     </div>
